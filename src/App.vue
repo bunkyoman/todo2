b497d6a62bb6a4bd178ece005fbe5ce677dbc077
bunkyoman <qnszy1347@gmail.com>
2023-11-17 11:41:18 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+      if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    },
+  methods: {
+    addTodo(todoItem) {
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+        localStorage.removeItem(todoItem);
+        this.todoItems.splice(index, 1);
+      }
+  },
+  components: {
+    "TodoHeader": TodoHeader,
+    "TodoInput": TodoInput,
+    "TodoList": TodoList,
+    "TodoFooter": TodoFooter,
+}
+}
+</script>
+
+<style scoped>
+
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+
+</style>
